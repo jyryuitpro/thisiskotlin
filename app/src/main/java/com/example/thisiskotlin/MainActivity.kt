9fe1449f5ae11016789ac4b0ef0460e9caac5a7f
jyr <jyr@wavers.co.kr>
2022-07-23 19:36:12 +0900

    [코틀린 안드로이드 기초 강의_4] 안드로이드 앱 개발을 위해서 꼭 알아야할 코틀린 문법과 사용법

--- app/src/main/java/com/example/thisiskotlin/MainActivity.kt
+++ app/src/main/java/com/example/thisiskotlin/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.thisiskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var myName = "류지영"
+        Log.d(TAG, "my name=$myName")
+        myName = "코틀린"
+        Log.d(TAG, "my name=$myName")
+        val PI = 3.141592
+        Log.d(TAG, "PI=$PI")
+
+        var myNumbers = "1,2,3,4,5,6"
+//        var thisWeekNumbers = "5,6,7,8,9,10"
+        var thisWeekNumbers = "1,2,3,4,5,6"
+        if (myNumbers == thisWeekNumbers) {
+            Log.d(TAG, "당첨되었습니다")
+//            textLog.text = "당첨되었습니다"
+        } else {
+            Log.d(TAG, "당첨되지 않았습니다")
+//            textLog.text = "당첨되지 않았습니다"
+        }
+
+        for (index in 1..10) {
+//            textLog.append("\n현재 숫자는 ${index}입니다")
+            Log.d(TAG, "현재 숫자는 ${index}입니다")
+        }
     }
 }
