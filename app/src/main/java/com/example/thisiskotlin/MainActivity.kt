283eeb95d3ff2a437fb04b0484890905a8cd02c5
jyr <jyr@wavers.co.kr>
2022-07-23 20:01:23 +0900

    [코틀린 안드로이드 기초 강의_5] 변수와 상수

--- app/src/main/java/com/example/thisiskotlin/MainActivity.kt
+++ app/src/main/java/com/example/thisiskotlin/MainActivity.kt
@@ -32,5 +32,37 @@ class MainActivity : AppCompatActivity() {
 //            textLog.append("\n현재 숫자는 ${index}입니다")
             Log.d(TAG, "현재 숫자는 ${index}입니다")
         }
+
+        var variable: String = "홍길동"
+
+        var variable2: String
+        variable2 = "안녕하세요"
+
+//        variable = 123
+        variable = "다른값"
+
+        // 코틀린의 기본타입
+        // 실수형
+        var doubleValue: Double = 35.6
+//        var floatValue: Float = 123.7F
+        // 정수형
+//        var intValue: Int = 12345678
+        var longValue: Long = 32_147_483_647L
+//        var shortValue: Short = 123
+//        var byteValue: Byte = 127
+        // 문자형
+        var charValue: Char = 'a'
+        var stringValue: String = "문자형 길이에 상관없이 값을 입력할 수 있다 == 크기를 특정할 수 없다."
+        // 불린형
+        var booleanValue: Boolean = true
+
+        booleanValue = false
+
+        val VALVALUE: Boolean = true
+
+//        valValue = false
+
+        var helloWorld = "안녕 세상아!"
+        var hello_World = "안녕 세상아!"
     }
 }
